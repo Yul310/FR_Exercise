30c65345e35adc224ff847ef65e693e49383b66f
Yul3102 <hsy3102@gmail.com>
2022-10-30 19:39:54 -0400

    confirmation page added and CSS started

--- server.js
+++ server.js
@@ -4,49 +4,40 @@ const PORT = 3000
 const app = require("liquid-express-views")(express());
 
 
+/////////////////////////////////////////////////////
+/// Middleware
+/////////////////////////////////////////////////////
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"))
 
+
+
+
+
+/////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////
 
 app.get('/', function(req, res){
-   
-
     async function getData(url = '') {
-    const response = await fetch(url).then((apiResponse) => {
-        console.log('hi')
-        return apiResponse.json()
-        // .then((jsonData) => {console.log(jsonData)})
-    
-    })
-    .catch((error) => {
-        console.log(error);
-      })
-      console.log(response)
-      let occupations = response.occupations
-      let states = response.states
-      res.render('creation',{occupations,states})
-  }
-  getData('https://frontend-take-home.fetchrewards.com/form')
+        const response = await fetch(url).then((apiResponse) => {
+            return apiResponse.json()   
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        console.log(response)
+        let occupations = response.occupations
+        let states = response.states
+        res.render('creation',{occupations,states})
+    }
+    getData('https://frontend-take-home.fetchrewards.com/form')
 })
 
 
 app.post('/create', async (req, res)=>{
-
-
     console.log(req.body)
-
     let data = req.body
-
-    // let data = {
-    //     name: 'sam',
-    //     email: 'sam@email.com',
-    //     password: 'tomato123',
-    //     occupation: 'Head of Shrubbery',
-    //     state: {name: 'North Dakota', abbreviation: 'ND'}
-      
-    // }
-
-
-
     const response = await fetch('https://frontend-take-home.fetchrewards.com/form', {
       method: 'POST',
       headers: {
@@ -56,18 +47,15 @@ app.post('/create', async (req, res)=>{
       body: JSON.stringify(data) 
     })
     .then((apiResponse) => {
-        console.log('return')
         return apiResponse.json()
-       
     })
     .catch((error) => {
         console.log(error);
         response.json({ error });
-      });
+    })
     console.log(response)
-    res.redirect('https://frontend-take-home.fetchrewards.com/form')
-
-
+    res.render('confirmation',{response})
+    // res.redirect('/confirmation')
 })
 
 
@@ -79,13 +67,6 @@ app.listen(PORT, () => {
 })
 
 
-let data = {
-    name: 'myname',
-    email: 'name@email.com',
-    password: 'password',
-    occupation: 'occupation',
-    state: 'state',
-}
 
 
 // async function postData(url = '', data = {}) {
